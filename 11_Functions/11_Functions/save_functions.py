5ba031dd2fa16351ba8453f4cc6f1e6a5bbbc1d9
AHosseinzadehA <104752674+AHosseinzadehA@users.noreply.github.com>
2025-08-25 17:54:31 +0330

    Delete 11_Functions directory

--- 11_Functions/11_Functions/save_functions.py
+++
@@ -1,6 +0,0 @@
-
-def tax_calculator(subtotal: float, tax_rate: float) -> float:
-    tax = round(subtotal * tax_rate, 2)
-    total = round(subtotal + tax, 2)
-    
-    return [subtotal, tax, total]
